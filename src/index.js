93ace27b75366656ae28fc8e4a92a49cacea6b41
Meenal Mathur <meemathu@publicisgroupe.net>
2021-07-07 10:35:52 +0530

    Commit by Meenal

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const http = require('http'); // Offer Carousel Slideshow
+
+
+
+
+//Plp-tablet layout
+const mainSection = document.createElement('section');
+const sidebar = document.createElement('div');
+listItem = document.createElement('li');

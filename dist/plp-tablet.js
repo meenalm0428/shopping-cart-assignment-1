bd2722709e1eb8bb946b19fb294443ae2e1b3a0d
Meenal Mathur <meemathu@publicisgroupe.net>
2021-07-14 10:10:10 +0530

    Commit by Meenal Mathur

--- dist/plp-tablet.js
+++ dist/plp-tablet.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63319" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62118" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
